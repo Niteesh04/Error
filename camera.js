4ad0257c22cf4ccbb373bc9ce6841eb2f693dd62
Niteesh Singh <76947571+Niteesh04@users.noreply.github.com>
2022-07-05 17:35:26 +0530

    Add files via upload

---
+++ camera.js
@@ -0,0 +1,71 @@
+import * as React from "react"
+import { Button,Image,View,Platform } from "react-native"
+import * as ImagePicker from "expo-image-picker"
+import * as Permissions from "expo-permissions"
+
+export default class PickImage extends React.Component{
+    state={
+        image:null
+    }
+getPermissionsAsync=async()=>{
+    if(Platform.OS !== "web"){
+        const {status}=await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        if(status !== "granted"){
+            alert("Sorry, we need camera roll permissions to make this work..")
+        }
+    }
+}
+componentDidMount(){
+    this.getPermissionsAsync();
+}
+_pickImage=async()=>{
+    try{
+        let result=await ImagePicker.launchImageLibraryAsync({
+            mediaTypes:ImagePicker.MediaTypeOptions.All,
+            allowsEditing:true,
+            aspect:[4,3],
+            quality:1
+        })
+        if(!result.cancelled){
+            this.setState({image:result.data})
+            console.log(result.uri)
+            this.uploadImage(result.uri)
+        }
+    }
+    catch(E){
+        console.log(E)
+    }
+}
+uploadImage=async(uri)=>{
+    const data=new FormData();
+    let fileName=uri.split("/")[uri.split("/").length-1]
+    let Type=`image/${uri.split("/")[uri.split("/").length-1]}`
+    const fileToUpload={
+        uri:uri,
+        name:fileName,
+        type:type,
+    }
+data.append("digit",fileToUpload)
+fetch("https://5682-1-186-229-237.in.ngrok.io/predict-digit",{
+    method:"POST",
+    body:data,
+    headers:{
+        "content-type":"multiPart/form-data"
+    },
+})
+.then((response)=>response.json())
+.then((result)=>console.log("Success",result))
+.catch((error)=>console.error("error",error))
+}
+    render(){
+        let{image}=this.state;
+        return(
+            <View style={{flex:1,alignitems:"center",justifycontent:"center"}}>
+                <Button
+                title="Pick An Image From Camera Roll"
+                onPress={this._pickImage}
+                />
+            </View>
+        )
+    }
+}
